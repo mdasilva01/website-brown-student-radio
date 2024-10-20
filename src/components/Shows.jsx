131e13e8921a3e345bd6f60bd2c43d59f6d94f53
sjuknelis <sjuknelis24@nobles.edu>
2024-10-20 16:08:26 -0400

    custom shows box

---
+++ src/components/Shows.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from "react";
+import "./Shows.css";
+
+export default function Shows() {
+    const [shows, setShows] = useState(null);
+
+    const showsBox = useRef(null);
+    const [showsBoxOpen, setShowsBoxOpen] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("https://corsproxy.io/?https://programs.testradio.org/WBRU/");
+            const doc = new DOMParser().parseFromString(await response.text(), "text/html");
+
+            setShows(
+                Array.from(doc.getElementById("w0")
+                    .getElementsByTagName("tr"))
+                    .map(row => Array.from(row.children)
+                        .map(col => col.innerText)));
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (showsBox.current) {
+            const factor = showsBoxOpen ? 1 : 0.2;
+            showsBox.current.style.maxHeight = showsBox.current.scrollHeight * factor + "px";
+        }
+    }, [showsBox.current, showsBoxOpen]);
+
+    if (shows) {
+        return (
+            <>
+                <h3>Shows coming up</h3>
+                <div className="shows-box" ref={showsBox} onClick={() => setShowsBoxOpen(!showsBoxOpen)}>
+                    <table className="shows-table">
+                        {shows.map(show => (
+                            <tr>
+                                <td>{show[0]}</td>
+                                <td>{show[1]}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            </>
+        );
+    } else {
+        return null;
+    }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import './Home.css';
 import Player from '../components/Player';
+import Shows from '../components/Shows';
 
 const About = () => {
   return (
     <div id="home">
+      <br />
       <h3>
         Click to listen to <span className="accent">101.1 FM LP-Providence</span>
       </h3>
       <h1>Brown Student & Community Radio</h1>
       <Player />
+      <br />
+      <Shows />
     </div>
   );
 };
