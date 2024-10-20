359c273f6da9b7f2257fc227c8f10b470f0ae0ce
sjuknelis <sjuknelis24@nobles.edu>
2024-10-19 23:26:42 -0400

    fun(?) radio spinner player

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import './Home.css';
+import Player from '../components/Player';
 
-const About: React.FC = () => {
+const About = () => {
   return (
-    <h1>Home page!</h1>
+    <div id="home">
+      <h3>
+        Click to listen to <span className="accent">101.1 FM LP-Providence</span>
+      </h3>
+      <h1>Brown Student & Community Radio</h1>
+      <Player />
+    </div>
   );
 };
 
