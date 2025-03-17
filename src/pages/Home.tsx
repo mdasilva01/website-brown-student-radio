8163e1ea15baf5a34bf3504ff910070aa8e8f176
mdasilva01 <matthewdasilva001@gmail.com>
2025-03-17 17:33:14 -0400

    Try again

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,7 +41,7 @@ export default function Home() {
             <div className="text-wrapper">
                 <img 
                     className={`play-gif ${loading ? "spin" : ""}`} 
-                    src="/play-button.gif" 
+                    src="/icons8-play.gif" 
                     onClick={togglePlaying} 
                     alt="Play Button" 
                 />
